e1f8b0d814161363a1d7b64775201f52b13a22b2
Duc Pham <phamvoduc.it1905@gmail.com>
2025-03-11 17:05:04 +0000

    cahnge

--- ThucHanh/AtHome/W1/Ptr_Exercise_2/Ptr_Exercise_2.cpp
+++ ThucHanh/AtHome/W1/Ptr_Exercise_2/Ptr_Exercise_2.cpp
@@ -50,11 +50,13 @@ void inputArrayOfFraction(Fraction *&fraction, int &n) {
     }
 }
 
-void ouputArrayOfFraction(Fraction *fraction, int n) {
+
+void outputArrayOfFraction(Fraction *fraction, int n) {
     for (int i = 0; i < n; i++) {
         cout << "Fraction " << i + 1 << ": " << fraction[i] << endl;
     }
 }
+
 
 void insertFractionAtEnd(Fraction *&fraction, int &n, Fraction value) {
     Fraction *newFraction = new Fraction[n + 1];
@@ -67,12 +69,43 @@ void insertFractionAtEnd(Fraction *&fraction, int &n, Fraction value) {
     fraction = newFraction;
 }
 
+
+void deleteFractionAtEnd(Fraction *&fraction, int &n){
+    Fraction *newFraction = new Fraction[n - 1];
+    for (int i = 0; i < n-1; i++) {
+        newFraction[i] = fraction[i];
+    }
+    n--;
+    delete[] fraction;
+    fraction = newFraction;
+}
+
+void delocateFractionArray(Fraction *&fraction) {
+    delete[] fraction;
+    fraction = nullptr;
+}
+
 int main(int argc, char const *argv[])
 {
+    Fraction *fraction = nullptr;
+    int n;
+    inputArrayOfFraction(fraction, n);
+    outputArrayOfFraction(fraction, n);
+
     Fraction *fractionPtr = new Fraction; 
+    cout << "Enter a new fraction to at at end: ";
     cin >> (*fractionPtr);  
-    cout << (*fractionPtr); 
+    insertFractionAtEnd(fraction, n, *fractionPtr);
+    outputArrayOfFraction(fraction, n);
+
+    cout << "Delete a fraction at end: " << endl;
+    deleteFractionAtEnd(fraction, n);
+    outputArrayOfFraction(fraction, n);
+
+
+    delocateFractionArray(fraction);
     delete fractionPtr; 
 
     return 0;
 }
+
