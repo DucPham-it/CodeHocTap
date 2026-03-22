063d7f9cfff689ac939753d20b8525933e22da8e
DucPham-it <phamvoduc.it1905@gmail.com>
2026-03-23 02:01:53 +0700

    Pre - updating

--- Thuchanh/W1/textFile/main.cpp
+++ Thuchanh/W1/textFile/main.cpp
@@ -8,49 +8,50 @@ using namespace std;
 struct Line {
     int nwords = 0;
     string line;
-    Line* nLines = nullptr;
+    Line *nLines = nullptr;
 };
 struct TFile {
     int nlines = 0;
-    Line* lines = nullptr;
+    Line *lines = nullptr;
 };
 
-void readTFile(string nameFile, TFile& output);
+void readTFile(string nameFile, TFile &output);
 void deleteTFile(TFile nameFile);
 int numInLine(Line line);
 void writeInBynary(string nameFile, TFile input);
 
-int main(int argc, char *argv[]) { 
+int main(int argc, char *argv[]) {
     string nameFile = argv[1];
     TFile text;
 
     readTFile(nameFile, text);
-    Line* head = text.lines;
+    Line *head = text.lines;
 
     cout << "N lines in fiel: " << text.nlines << "\n";
 
     while (head != nullptr) {
-        cout << head->line << " n wolds in line: "<< head->nwords <<"\n";
+        cout << head->line << " n wolds in line: " << head->nwords << "\n";
         head = head->nLines;
     }
 
     deleteTFile(text);
-    
-    return 0; }
 
-int numInLine(Line* line){
+    return 0;
+}
+
+int numInLine(Line *line) {
     string token;
     int count = 0;
-    
+
     stringstream ss(line->line);
 
-    while (ss >> token){
+    while (ss >> token) {
         count++;
     }
     return count;
 }
 
-void readTFile(string nameFile, TFile& output) {
+void readTFile(string nameFile, TFile &output) {
     ifstream input;
     input.open(nameFile);
 
@@ -59,16 +60,15 @@ void readTFile(string nameFile, TFile& output) {
     }
 
     string line;
-    Line* hold;
+    Line *hold;
 
     while (getline(input, line)) {
-        Line* temp = new Line;
+        Line *temp = new Line;
         temp->line = line;
         temp->nwords = numInLine(temp);
         if (output.lines == nullptr) {
             output.lines = temp;
-        }
-        else {
+        } else {
             hold->nLines = temp;
         }
         output.nlines++;
@@ -77,27 +77,26 @@ void readTFile(string nameFile, TFile& output) {
     input.close();
 }
 
-void deleteTFile(TFile nameFile){
-    Line* head = nameFile.lines;
+void deleteTFile(TFile nameFile) {
+    Line *head = nameFile.lines;
 
     while (head->nLines != nullptr) {
-        Line* temp = head;
+        Line *temp = head;
         head = head->nLines;
         delete temp;
     }
 }
 
-void writeInBynary(string nameFile, TFile input){
+void writeInBynary(string nameFile, TFile input) {
     ofstream output(nameFile, ios::binary);
-
 
     if (!output.is_open()) {
         cout << "Can't open file \n";
     }
-    Line* head = input->lines;
-    
+    Line *head = input->lines;
+
     while (head != nullptr) {
-        
+
         head = head->nLines;
     }
 }
